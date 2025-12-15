0cc4fb4f4472e520c12405dfbaf7677d8e237c1d
develper21 <githubnarvin@gmail.com>
2025-12-15 23:19:09 +0530

    Update App.tsx - improve main application structure and routing

--- leetcheat/src/App.tsx
+++ leetcheat/src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from '@/components/UI/sonner';
 import { TooltipProvider } from '@/components/UI/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'next-themes';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 
@@ -9,15 +10,20 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <ThemeProvider defaultTheme="system" enableSystem>
+      <TooltipProvider>
+        <Toaster />
+        <BrowserRouter future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true
+        }}>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
