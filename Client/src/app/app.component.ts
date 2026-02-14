ec0713688a3ba9ad1e2fea100d8202a09640a178
Narvin Sachaniya <githubnarvin@gmail.com>
2026-02-14 11:35:59 +0530

    Add app.component.ts

---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ThemeService } from './services/theme.service';
+import { AuthService } from './services/auth.service';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'algo-bytes-web-app';
+
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService,
+    public dataService: DataService
+  ) {
+    this.authService.getUser();
+  }
+
+  changeTheme() {
+    this.themeService.toggleTheme();
+  }
+}
